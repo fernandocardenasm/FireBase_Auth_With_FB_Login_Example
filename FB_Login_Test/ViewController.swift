5fc55ceae72adc4ef6f0d6035bc8d4d2b5359157
fernandocardenasm <fernando.cardenasm@gmail.com>
2016-07-13 22:14:01 +0200

    Add Login, Logout FB Session

--- FB_Login_Test/ViewController.swift
+++ FB_Login_Test/ViewController.swift
@@ -8,11 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    let loginButton: FBSDKLoginButton = {
+       let button = FBSDKLoginButton()
+        button.readPermissions = ["email"]
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(loginButton)
+        loginButton.center = view.center
+        loginButton.delegate = self
+        
+        if let token = FBSDKAccessToken.currentAccessToken(){
+            fetchProfile()
+        }
+    }
+    
+    func fetchProfile(){
+        print("Fetch Profile")
+        
+        let parameters = ["fields": "email, first_name, last_name, picture.type(large)"]
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).startWithCompletionHandler { (connection, user, error) in
+            
+            if error != nil {
+                print(error)
+                return
+            }
+            
+            if let first_name = user["first_name"] as? String{
+                print("first_name")
+                print(first_name)
+            }
+            
+            if let picture = user["picture"] as? NSDictionary, data = picture["data"] as? NSDictionary, url = data["url"] as? String {
+                print("url")
+                print(url)
+            }
+        }
+    }
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        print("Completed login")
+        fetchProfile()
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("Completed logout")
+    }
+    func loginButtonWillLogin(loginButton: FBSDKLoginButton!) -> Bool {
+        return true;
     }
 
     override func didReceiveMemoryWarning() {
