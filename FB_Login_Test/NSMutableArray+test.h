8bddea2c4d7d2063625092589830a705271c4cc2
fernandocardenasm <fernando.cardenasm@gmail.com>
2016-07-13 21:18:14 +0200

    Add Bacis Settings for FB SDK Integration

---
+++ FB_Login_Test/NSMutableArray+test.h
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray+test.h
+//  FB_Login_Test
+//
+//  Created by Fernando Cardenas on 13/07/16.
+//  Copyright © 2016 Fernando. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableArray (test)
+
+@end
---
+++ FB_Login_Test/NSMutableArray+test.m
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray+test.m
+//  FB_Login_Test
+//
+//  Created by Fernando Cardenas on 13/07/16.
+//  Copyright © 2016 Fernando. All rights reserved.
+//
+
+#import "NSMutableArray+test.h"
+
+@implementation NSMutableArray (test)
+
+@end
